342b2bec2eb248981c47d4a637da9f1a12c25d88
bharathchandu302 <bharathchandu302@gmail.com>
2025-11-06 21:06:22 +0530

    aading accept

--- peering.tf
+++ peering.tf
@@ -2,4 +2,12 @@ resource "aws_vpc_peering_connection" "default" {
     count = var.is_peering_required ? 0 : 1
   peer_vpc_id   = data.aws_vpc.default # acceptor 
   vpc_id        = aws_vpc.main.id
+
+   accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
 }
